d0e6bc854c32222e8eb3e412fef3d6b227a06252
In joon. Gong <ijgong@nglp.kr>
2018-06-15 17:15:05 +0900

    navigation 수정 중  - TODO :: @Input을 통해 데이터가 넘어왔을 경우 작업이 generateNavMenu가 수행되도록 기능 수정 필요

--- nodejs/angular/first-app/src/app/nav-list/nav-list.component.ts
+++ nodejs/angular/first-app/src/app/nav-list/nav-list.component.ts
@@ -8,43 +8,46 @@ import { NavItem } from '../nav-item.model';
 })
 export class NavListComponent {
   @Input() navList: Array<NavItem>;
+  menuList: Array<NavItem>;
 
   constructor() {
     this.generateNavMenu();
   }
 
   generateNavMenu(): Array<NavItem> {
-    let menuList: Array<NavItem> = new Array<NavItem>();
+    this.menuList = new Array<NavItem>();
     for(let idx in this.navList){
       let navItem: NavItem = this.generateNavItem(this.navList, this.navList[idx]);
-      menuList.push(navItem);
+      this.menuList.push(navItem);
     }
-    console.log(menuList);
-    return menuList;
+    console.log(this.menuList);
+    return this.menuList;
   }
 
   // 메뉴리스트에서 isParent, parent를 체크하여 navigation을 생성
-  generateNavItem(navList: Array<NavItem>, navItem: NavItem): NavItem {
+  generateNavItem(pNavList: Array<NavItem>, pNavItem: NavItem): NavItem {
     // sub메뉴를 담는 변수
     let sub: Array<NavItem> = new Array<NavItem>();
     // 부모메뉴일 경우(sub메뉴가 존재하는 메뉴일 경우)
-    if(navItem.isParent){
+    if(pNavItem.isParent){
       // 현재 메뉴의 메뉴ID를 get
-      let id: string = navItem.id;
+      let id: string = pNavItem.id;
       // 전체 메뉴리스트를 검색해서 parent의 값이 ID와 같은 데이터를 sub에 담아 sub Navigation을 생성
       // 만일 sub메뉴의 isParent가 true(하위메뉴가 있을 경우)일 경우 재귀호출을 수행한다.
-      for(let idx in navList){
-        let subNav: NavItem = navList[idx];
+      for(let idx in pNavList){
+        let subNav: NavItem = pNavList[idx];
         if(subNav.parent == id){
           if(subNav.isParent){
-            subNav = this.generateNavItem(navList, subNav);
+            subNav = this.generateNavItem(pNavList, subNav);
           }
           sub.push(subNav);
+          delete pNavList[idx];
         }
       }
-      navItem.sub = sub;
+      pNavItem.sub = sub;
     }
-    return navItem;
+    this.navList = pNavList;
+    return pNavItem;
   }
 
 }
