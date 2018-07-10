6c905462a4ddd82e573ca951f9e95aff9bbff530
devilkij77 <devilkij77@gmail.com>
2018-07-10 23:47:00 +0900

    navigation component로 추출

--- nodejs/Vuejs/dashboard-app/src/components/HelloWorld.vue
+++ nodejs/Vuejs/dashboard-app/src/components/HelloWorld.vue
@@ -2,208 +2,8 @@
 
 <div>
   <!-- Navigation-->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top" id="mainNav">
-    <a class="navbar-brand" href="index.html">Start Bootstrap</a>
-    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-    <div class="collapse navbar-collapse" id="navbarResponsive">
-      <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
-          <a class="nav-link" href="index.html">
-              <font-awesome-icon icon="table" />
-              <!-- <i class="fa fa-fw fa-dashboard"></i> -->
-              <span class="nav-link-text">Dashboard</span>
-            </a>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Charts">
-          <a class="nav-link" href="charts.html">
-              <font-awesome-icon icon="chart-area"/>
-              <span class="nav-link-text">Charts</span>
-            </a>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Tables">
-          <a class="nav-link" href="tables.html">
-            <font-awesome-icon icon="table"/>
-              <span class="nav-link-text">Tables</span>
-            </a>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Components">
-          <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseComponents" data-parent="#exampleAccordion">
-              <font-awesome-icon icon="wrench"/>
-              <span class="nav-link-text">Components</span>
-            </a>
-          <ul class="sidenav-second-level collapse" id="collapseComponents">
-            <li>
-              <a href="navbar.html">Navbar</a>
-            </li>
-            <li>
-              <a href="cards.html">Cards</a>
-            </li>
-          </ul>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Example Pages">
-          <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseExamplePages" data-parent="#exampleAccordion">
-              <font-awesome-icon icon="file"/>
-              <span class="nav-link-text">Example Pages</span>
-            </a>
-          <ul class="sidenav-second-level collapse" id="collapseExamplePages">
-            <li>
-              <a href="login.html">Login Page</a>
-            </li>
-            <li>
-              <a href="register.html">Registration Page</a>
-            </li>
-            <li>
-              <a href="forgot-password.html">Forgot Password Page</a>
-            </li>
-            <li>
-              <a href="blank.html">Blank Page</a>
-            </li>
-          </ul>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Menu Levels">
-          <a class="nav-link nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti" data-parent="#exampleAccordion">
-              <font-awesome-icon icon="sitemap"/>
-              <span class="nav-link-text">Menu Levels</span>
-            </a>
-          <ul class="sidenav-second-level collapse" id="collapseMulti">
-            <li>
-              <a href="#">Second Level Item</a>
-            </li>
-            <li>
-              <a href="#">Second Level Item</a>
-            </li>
-            <li>
-              <a href="#">Second Level Item</a>
-            </li>
-            <li>
-              <a class="nav-link-collapse collapsed" data-toggle="collapse" href="#collapseMulti2">Third Level</a>
-              <ul class="sidenav-third-level collapse" id="collapseMulti2">
-                <li>
-                  <a href="#">Third Level Item</a>
-                </li>
-                <li>
-                  <a href="#">Third Level Item</a>
-                </li>
-                <li>
-                  <a href="#">Third Level Item</a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Link">
-          <a class="nav-link" href="#">
-              <font-awesome-icon icon="link"/>
-              <span class="nav-link-text">Link</span>
-            </a>
-        </li>
-      </ul>
-      <ul class="navbar-nav sidenav-toggler">
-        <li class="nav-item">
-          <a class="nav-link text-center" id="sidenavToggler">
-              <font-awesome-icon icon="angle-left"/>
-            </a>
-        </li>
-      </ul>
-      <ul class="navbar-nav ml-auto">
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle mr-lg-2" id="messagesDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <font-awesome-icon icon="envelope"/>
-              <span class="d-lg-none">Messages
-                <span class="badge badge-pill badge-primary">12 New</span>
-              </span>
-              <span class="indicator text-primary d-none d-lg-block">
-                <font-awesome-icon icon="circle"/>
-              </span>
-            </a>
-          <div class="dropdown-menu" aria-labelledby="messagesDropdown">
-            <h6 class="dropdown-header">New Messages:</h6>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-                <strong>David Miller</strong>
-                <span class="small float-right text-muted">11:21 AM</span>
-                <div class="dropdown-message small">Hey there! This new version of SB Admin is pretty awesome! These messages clip off when they reach the end of the box so they don't overflow over to the sides!</div>
-              </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-                <strong>Jane Smith</strong>
-                <span class="small float-right text-muted">11:21 AM</span>
-                <div class="dropdown-message small">I was wondering if you could meet for an appointment at 3:00 instead of 4:00. Thanks!</div>
-              </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-                <strong>John Doe</strong>
-                <span class="small float-right text-muted">11:21 AM</span>
-                <div class="dropdown-message small">I've sent the final files over to you for review. When you're able to sign off of them let me know and we can discuss distribution.</div>
-              </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item small" href="#">View all messages</a>
-          </div>
-        </li>
-        <li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle mr-lg-2" id="alertsDropdown" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              <font-awesome-icon icon="bell"/>
-              <span class="d-lg-none">Alerts
-                <span class="badge badge-pill badge-warning">6 New</span>
-              </span>
-              <span class="indicator text-warning d-none d-lg-block">
-                <font-awesome-icon icon="circle"/>
-              </span>
-            </a>
-          <div class="dropdown-menu" aria-labelledby="alertsDropdown">
-            <h6 class="dropdown-header">New Alerts:</h6>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-                <span class="text-success">
-                  <strong>
-                    <font-awesome-icon icon="long-arrow-alt-up"/>Status Update</strong>
-                  </span>
-                  <span class="small float-right text-muted">11:21 AM</span>
-                  <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
-                </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-                  <span class="text-danger">
-                    <strong>
-                      <font-awesome-icon icon="long-arrow-alt-down"/>Status Update</strong>
-                    </span>
-                    <span class="small float-right text-muted">11:21 AM</span>
-                    <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
-                  </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item" href="#">
-                    <span class="text-success">
-                      <strong>
-                        <font-awesome-icon icon="long-arrow-alt-up"/>Status Update</strong>
-                      </span>
-                      <span class="small float-right text-muted">11:21 AM</span>
-                      <div class="dropdown-message small">This is an automated server response message. All systems are online.</div>
-                    </a>
-            <div class="dropdown-divider"></div>
-            <a class="dropdown-item small" href="#">View all alerts</a>
-          </div>
-        </li>
-        <li class="nav-item">
-          <form class="form-inline my-2 my-lg-0 mr-lg-2">
-            <div class="input-group">
-              <input class="form-control" type="text" placeholder="Search for...">
-              <span class="input-group-append">
-                        <button class="btn btn-primary" type="button">
-                          <font-awesome-icon icon="search"/>
-                        </button>
-                      </span>
-            </div>
-          </form>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" data-toggle="modal" data-target="#exampleModal">
-            <font-awesome-icon icon="sign-out-alt"/>Logout</a>
-        </li>
-      </ul>
-    </div>
-  </nav>
+  <navigation></navigation>
+  <!-- END navigation -->
   <div class="content-wrapper">
     <div class="container-fluid">
       <!-- Breadcrumbs-->
@@ -1086,12 +886,17 @@
 </template>
 
 <script>
+import Navigation from '@/components/Navigation'
+
 export default {
   name: 'HelloWorld',
   data: function () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components: {
+    Navigation
   }
 }
 </script>
